f8e222faa5e9235adf5669166225a11ad6bfc9ad
Alex <alex.nektony@gmail.com>
2021-05-09 22:50:16 +0300

    empty project

---
+++ CollageMaker/CollageMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollageMaker
+//
+//  Created by Alex on 5/9/21.
+//  Copyright © 2021 AlexCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
